fe83a4ca895ad1f73a8b034506a85edcf8ed8ccd
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-04-26 18:44:47 -0700

    Move common test utils into fixtures

---
+++ tests/model/conftest.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pytest
+
+import lfmc
+from galileo.data.config import NORMALIZATION_DICT_FILENAME
+from galileo.data.dataset import Dataset, Normalizer
+from galileo.galileo import Encoder
+
+ROOT_DIR = Path(lfmc.__file__).parent.parent
+
+
+@pytest.fixture
+def config_dir() -> Path:
+    return ROOT_DIR / "data" / "config"
+
+
+@pytest.fixture
+def normalizer(config_dir: Path) -> Normalizer:
+    normalization_dicts = Dataset.load_normalization_values(config_dir / NORMALIZATION_DICT_FILENAME)
+    return Normalizer(std=True, normalizing_dicts=normalization_dicts)
+
+
+@pytest.fixture
+def encoder() -> Encoder:
+    return Encoder.load_from_folder(Path(ROOT_DIR / "data" / "models" / "nano"))
+
+
+@pytest.fixture
+def data_folder() -> Path:
+    current_dir = Path(__file__).parent
+    return Path(current_dir / "data")
+
+
+@pytest.fixture
+def h5py_folder(tmp_path: Path) -> Path:
+    h5py_folder = tmp_path / "h5pys"
+    h5py_folder.mkdir(parents=True, exist_ok=True)
+    return h5py_folder
