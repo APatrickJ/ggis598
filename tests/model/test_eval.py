37d6e0fb746e74ebcfa328e30a1fc12f84a42374
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-04-26 18:44:47 -0700

    Move common test utils into fixtures

--- tests/model/test_eval.py
+++ tests/model/test_eval.py
@@ -1,34 +1,19 @@
 from pathlib import Path
 
-import lfmc
-from galileo.data.config import NORMALIZATION_DICT_FILENAME
-from galileo.data.dataset import Dataset, Normalizer
+from galileo.data.dataset import Normalizer
 from galileo.galileo import Encoder
 from lfmc.model.eval import FinetuningConfig, LFMCEval
 
-ROOT_DIR = Path(lfmc.__file__).parent.parent
 
-
-def load_normalizer(config_dir: Path) -> Normalizer:
-    normalization_dicts = Dataset.load_normalization_values(config_dir / NORMALIZATION_DICT_FILENAME)
-    return Normalizer(std=True, normalizing_dicts=normalization_dicts)
-
-
-def test_finetune(tmp_path: Path):
-    config_dir = ROOT_DIR / "data" / "config"
-    current_dir = Path(__file__).parent
-    data_folder = Path(current_dir / "data")
-    h5py_folder = tmp_path / "h5pys"
-    h5py_folder.mkdir(parents=True, exist_ok=True)
+def test_finetune(tmp_path: Path, normalizer: Normalizer, encoder: Encoder, data_folder: Path, h5py_folder: Path):
     output_folder = tmp_path / "finetuned"
     output_folder.mkdir(parents=True, exist_ok=True)
     lfmc_eval = LFMCEval(
-        normalizer=load_normalizer(config_dir),
+        normalizer=normalizer,
         data_folder=data_folder,
         h5py_folder=h5py_folder,
         h5pys_only=False,
     )
-    encoder = Encoder.load_from_folder(Path(ROOT_DIR / "data" / "models" / "nano"))
     finetuning_config = FinetuningConfig(
         max_epochs=1,
         weight_decay=0.001,
