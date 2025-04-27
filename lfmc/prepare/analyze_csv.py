567443b7d288ec581283c50b04fc238233b33f51
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-04-26 18:18:02 -0700

    Analyze aggregated data, output more metrics

--- lfmc/prepare/analyze_csv.py
+++ lfmc/prepare/analyze_csv.py
@@ -2,28 +2,40 @@ import argparse
 import logging
 from pathlib import Path
 
-import pandas as pd
-
-from lfmc.common.const import LABELS_PATH
+from lfmc.common.const import LABELS_PATH, METEOROLOGICAL_SEASON_MONTHS, Column
+from lfmc.common.labels import read_labels
 
 
 def analyze_csv(input_csv_path: Path):
     logging.info("Reading the CSV file")
-    df = pd.read_csv(input_csv_path)
+    df = read_labels(input_csv_path)
 
     logging.info("Analyzing the CSV file")
     logging.info("Number of rows: %d", len(df))
-    logging.info("Number of sites: %d", df["site_name"].nunique())
-    logging.info("Number of species: %d", df["species_collected"].nunique())
-    logging.info("Number of functional types: %d", df["species_functional_type"].nunique())
-    logging.info("Median LFMC: %f", df["lfmc_value"].median())
-    logging.info("Min LFMC: %f", df["lfmc_value"].min())
-    logging.info("Max LFMC: %f", df["lfmc_value"].max())
+    logging.info("Number of sites: %d", df[Column.SITE_NAME].nunique())
+    logging.info("Median LFMC: %f", df[Column.LFMC_VALUE].median())
+    logging.info("Min LFMC: %f", df[Column.LFMC_VALUE].min())
+    logging.info("Max LFMC: %f", df[Column.LFMC_VALUE].max())
     logging.info(
         "Percentiles:\n%s",
-        df["lfmc_value"].quantile([0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 0.999, 0.9999]),
+        df[Column.LFMC_VALUE].quantile([0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 0.999, 0.9999]),
     )
-    logging.info("Protocols and counts:\n%s", df["protocol"].value_counts())
+    logging.info("State regions and counts:\n%s", df[Column.STATE_REGION].value_counts())
+    logging.info("Landcover classes and counts:\n%s", df[Column.LANDCOVER].value_counts())
+    logging.info("Elevation range: %d - %d", df[Column.ELEVATION].min(), df[Column.ELEVATION].max())
+    for i in range(0, 3500, 250):
+        logging.info(
+            "Elevation count in range %d - %d: %d",
+            i,
+            i + 250,
+            len(df[df[Column.ELEVATION].between(i, i + 250, inclusive="left")]),
+        )
+    for season in METEOROLOGICAL_SEASON_MONTHS.keys():
+        logging.info(
+            "Season %s count: %d",
+            season,
+            len(df[df[Column.SAMPLING_DATE].dt.month.isin(METEOROLOGICAL_SEASON_MONTHS[season])]),
+        )
 
 
 def main():
