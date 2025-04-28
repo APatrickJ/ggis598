9e617d9a13d0d131736c0c2af5d5bb23dc22ab04
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-04-28 13:31:39 -0700

    Add tests for pads_dates and fix a bug

--- lfmc/core/padding.py
+++ lfmc/core/padding.py
@@ -8,5 +8,5 @@ def pad_dates(end_date: date, padding: timedelta) -> tuple[date, date]:
     new_end_date = end_date + padding
     last_day_of_month = calendar.monthrange(new_end_date.year, new_end_date.month)[1]
     new_end_date = date(new_end_date.year, new_end_date.month, last_day_of_month)
-    start_date = date(end_date.year - 1, end_date.month, 1)
+    start_date = date(new_end_date.year - 1, new_end_date.month, 1)
     return start_date, new_end_date
